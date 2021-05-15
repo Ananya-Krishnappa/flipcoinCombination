07c5c4c1fe347b79b64cf1fe3142afcd8ce7bfa2
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-05-15 22:07:45 +0530

    Added:flipcoin dictionary

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-Flip=$(($(($RANDOM%10))%2))
-if [ $Flip -eq 1 ]
-then
-    echo "heads"
-else
-    echo "tails"
-fi
+n=20 
+declare -A coinsDict
+for((i=1; i<=$n; i++))
+do 
+	coinFace=Head
+	Flip=$(($(($RANDOM%10))%2))
+	if [ $Flip -eq 1 ]
+	then
+   	coinFace=Head
+	else
+   	coinFace=Tail
+	fi
+	coinsDict[$coinFace]=$((${coinsDict[$coinFace]}+1))
+done
+for coins in ${!coinsDict[@]}
+do
+	echo $coins $((${coinsDict[$coins]}*100/$n))"%"
+done
