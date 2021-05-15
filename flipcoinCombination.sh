563c5905edd06f807a592b030070636c4a05eb48
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-05-15 22:31:50 +0530

    Added:flipcoin doublet

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -3,13 +3,20 @@ n=20
 declare -A coinsDict
 for((i=1; i<=$n; i++))
 do 
-	coinFace=Head
+	coinFace=HH
 	Flip=$(($(($RANDOM%10))%2))
+	Flip1=$(($(($RANDOM%10))%2))
 	if [ $Flip -eq 1 ]
 	then
-   	coinFace=Head
+   	coinFace=H
 	else
-   	coinFace=Tail
+   	coinFace=T
+	fi
+	if [ $Flip1 -eq 1 ]
+	then
+		coinFace=$coinFace"H"
+	else
+		coinFace=$coinFace"T"
 	fi
 	coinsDict[$coinFace]=$((${coinsDict[$coinFace]}+1))
 done
